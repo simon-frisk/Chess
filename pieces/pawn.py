01612e8008984c38d888f55eb20d8b91e2256c3d
Simon Frisk <simon@simonfrisk.com>
2020-09-08 23:40:54 +0200

    Add feature : pawns can take two steps on their first move

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -6,5 +6,11 @@ class Pawn(Piece):
         super().__init__(board, column, row, piece_color, PieceType.PAWN)
 
     def calculate_possible_move_boxes(self):
+        possible_moves = []
         direction = -1 if self.piece_color == PieceColor.WHITE else 1
-        return [{'row': self.row + 1 * direction, 'column': self.column}]
+        if(self.piece_color == PieceColor.WHITE and self.row == self.board.BOX_COUNT - 2 or self.piece_color == PieceColor.BLACK and self.row == 1):
+            possible_moves += [{'row': self.row +
+                                2 * direction, 'column': self.column}]
+        possible_moves += [{'row': self.row +
+                            1 * direction, 'column': self.column}]
+        return possible_moves
