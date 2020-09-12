6b927d4cf9f743032813dbc15735e19de230eb20
Simon Frisk <simon@simonfrisk.com>
2020-09-12 14:51:43 +0200

    Begin implementing castling

--- pieces/king.py
+++ pieces/king.py
@@ -1,4 +1,5 @@
 from pieces.piece import Piece, PieceType, PieceColor
+import board
 
 
 class King(Piece):
@@ -15,4 +16,21 @@ class King(Piece):
         possible_moves += self.step_possible(pieces, 1, -1, 1)
         possible_moves += self.step_possible(pieces, -1, 1, 1)
         possible_moves += self.step_possible(pieces, -1, -1, 1)
+
+        castle_rooks = []
+        row = 0 if self.color == PieceColor.BLACK else 7
+        for piece in pieces:
+            if piece.color == self.color and piece.piece_type == PieceType.ROOK and not piece.has_moved:
+                castle_rooks.append(piece)
+
+        for rook in castle_rooks:
+            lowest_column = min(rook.column + 1, self.column + 1)
+            highest_column = max(rook.column, self.column)
+            for column in range(lowest_column, highest_column):
+                # TODO: King cannot be checked and cannot castle through attacked box. Rook can though
+                piece = board.get_piece(pieces, {'row': row, 'column': column})
+                if piece:
+                    continue
+                # add move
+
         return possible_moves
